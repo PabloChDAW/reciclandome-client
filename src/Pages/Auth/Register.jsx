333d4b809964923d9116878eab489d296f2bb753
PabloChDAW <pcg0031@alu.medac.es>
2025-03-27 13:07:23 +0100

    Crear formulario de registro en Register.jsx
    - Campos de entrada creados.

--- src/Pages/Auth/Register.jsx
+++ src/Pages/Auth/Register.jsx
@@ -2,6 +2,24 @@ export default function Register() {
   return (
     <>
       <h1 className="title">Regístrate</h1>
+      <form className="w-1/2 mx-auto space-y-6">
+        <div>
+          <input type="text" placeholder="Nombre" />
+        </div>
+
+        <div>
+          <input type="text" placeholder="Email" />
+        </div>
+
+        <div>
+          <input type="password" placeholder="Contraseña" />
+        </div>
+
+        <div>
+          <input type="password" placeholder="Confirmar contraseña" />
+        </div>
+        <button className="primary-btn">Registrarse</button>
+      </form>
     </>
   );
 }
