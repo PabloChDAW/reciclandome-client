6e6155bccdfbd37077fb4147c67f34614253de91
PabloChDAW <pcg0031@alu.medac.es>
2025-03-27 13:27:55 +0100

    Crear función para probar el formulario en consola
    Los datos introducidos en el formulario crean un objeto usando el hook useState. Muestra el objeto en consola para probar el funcionamiento.

--- src/Pages/Auth/Register.jsx
+++ src/Pages/Auth/Register.jsx
@@ -1,22 +1,58 @@
+import { useState } from "react";
+
 export default function Register() {
+  // Maneja el estado (valores) de los campos de entrada del formulario.
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password_confirmation: '',
+  });
+
+  function handleRegister(e) {
+    e.preventDefault();
+
+    console.log(formData);
+  }
+
   return (
     <>
       <h1 className="title">Regístrate</h1>
-      <form className="w-1/2 mx-auto space-y-6">
+      <form onSubmit={handleRegister} className="w-1/2 mx-auto space-y-6">
         <div>
-          <input type="text" placeholder="Nombre" />
+          <input
+            type="text"
+            placeholder="Nombre"
+            value={formData.name}
+            onChange={(e) => setFormData({ ...formData, name: e.target.value })}
+          />
         </div>
 
         <div>
-          <input type="text" placeholder="Email" />
+          <input
+            type="text"
+            placeholder="Email"
+            value={formData.email}
+            onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+          />
         </div>
 
         <div>
-          <input type="password" placeholder="Contraseña" />
+          <input
+            type="password"
+            placeholder="Contraseña"
+            value={formData.password}
+            onChange={(e) => setFormData({ ...formData, password: e.target.value })}
+          />
         </div>
 
         <div>
-          <input type="password" placeholder="Confirmar contraseña" />
+          <input
+            type="password"
+            placeholder="Confirmar contraseña"
+            value={formData.password_confirmation}
+            onChange={(e) => setFormData({ ...formData, password_confirmation: e.target.value })}
+          />
         </div>
         <button className="primary-btn">Registrarse</button>
       </form>
