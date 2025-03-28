edd501c6744b1f2587af5fd0b5d9c5717b12c090
PabloChDAW <pcg0031@alu.medac.es>
2025-03-28 11:43:17 +0100

    Implementar cierre de sesión
    Al llamar al endpoint logout de la API, se borra el token en el servidor. También se borra el token en el almacenamiento local, y tanto user como token pasan a ser null en todo el contexto de la aplicación cliente.

--- src/Pages/Layout.jsx
+++ src/Pages/Layout.jsx
@@ -1,9 +1,32 @@
 import { useContext } from "react";
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useNavigate } from "react-router-dom";
 import { AppContext } from "../Context/AppContext";
 
 export default function Layout() {
   const { user, token, setUser, setToken } = useContext(AppContext);
+  const navigate = useNavigate();
+
+  async function handleLogout(e) {
+    e.preventDefault();
+
+    /* Petición de logout */
+    const res = await fetch('/api/logout', {
+      method: "post",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      }
+    });
+
+    const data = await res.json();
+    console.log(data);
+
+    if(res.ok) {
+      setUser(null);
+      setToken(null);
+      localStorage.removeItem("token");
+      navigate('/');
+    }
+  }
 
   return (
     <>
@@ -14,7 +37,13 @@ export default function Layout() {
           </Link>
 
           {user ? (
-            <p className="text-slate-400 text-xs">{user.name}</p>
+            <div className="flex items-center space-x-4">
+              <p className="text-slate-400 text-xs">{user.name}</p>
+              <form onSubmit={handleLogout}>
+                <button className="nav-link">Cerrar sesión</button>
+              </form>
+            </div>
+            
           ) : (
             <div className="space-x-4">
             <Link to="/register" className="nav-link">
