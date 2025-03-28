ba8cf7ce3be1cd5f3782d20a0efb8841fc79c2e2
PabloChDAW <pcg0031@alu.medac.es>
2025-03-28 13:51:45 +0100

    Mostrar información sobre un punto específico
    - Usando el hook useParams podemos manipular la URL para acceder a la información de un point concreto por su id.
    - Se llama a la API a través de una URL dinámica incluyendo el id del point a cuyos datos se quiere acceder.

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Login from './Pages/Auth/Login';
 import { useContext } from 'react';
 import { AppContext } from './Context/AppContext';
 import Create from './Pages/Points/Create';
+import Show from './Pages/Points/Show';
 
 export default function App() {
   const { user } = useContext(AppContext);
@@ -19,6 +20,7 @@ export default function App() {
           <Route path='/register' element={user ? <Home /> : <Register />} />
           <Route path='/login' element={user ? <Home /> : <Login />} />
           <Route path='/create' element={user ? <Create /> : <Login />} />
+          <Route path='/points/:id' element={<Show />} />
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ src/Pages/Points/Show.jsx
@@ -0,0 +1,50 @@
+import { useContext, useEffect, useState } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import { AppContext } from "../../Context/AppContext";
+
+export default function Show() {
+  // console.log(useParams());
+  const {id} = useParams();
+  const navigate = useNavigate();
+  const { user, token } = useContext(AppContext);
+  const [point, setPoint] = useState(null);
+
+  async function getPoint() {
+    /* Petición de datos de un post. */
+    const res = await fetch(`/api/points/${id}`);
+    const data = await res.json();
+
+    // console.log(data);
+
+    if (res.ok) {
+      setPoint(data.point);
+    }
+  }
+
+  useEffect(() => {
+    getPoint();
+  }, []);
+
+  return (
+    <>
+      {point ? (
+        <div key={point.id} className="mt-4 p-4 border rounded-md border-dashed border-slate-400">
+          <div className="mb-2 flex items-start justify-between">
+            <div>
+              <p>{point.latitude}</p>
+              <p>{point.longitude}</p>
+              <small className="text-xs text-slate-600">
+                Creado por {point.user.name} a las{" "} {new Date(point.created_at).toLocaleTimeString()}
+              </small>
+            </div>
+          </div>
+
+          {user && user.id === point.user_id && <div className="flex items-center justify-end gap-4">
+          </div>}
+        </div>
+      ) : (
+        <p className="title">Punto no encontrado.</p>
+      )}
+    </>
+  );
+}
