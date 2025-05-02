882851a2ea1a6197be5638ad4ea6b34ca51ace4c
IvnCr <icr0015@alu.medac.es>
2025-05-02 18:48:21 +0200

    avances

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -135,9 +135,8 @@ export default function Header() {
                     <Link to="/contact" className="hover:underline hover:font-bold">Contacto</Link>
                 </nav>
 
-                {/* Auth Desktop */}
                 {/* Auth + Carrito Desktop */}
-                <div className="hidden md:flex items-center gap-6 text-black relative">
+                <div className="hidden md:flex items-center gap-6 text-[#131700] relative">
                     {user ? (
                         <>
                             <p className="text-md">Hola, {user.name}</p>
@@ -151,10 +150,20 @@ export default function Header() {
                     <div className="relative" ref={dropdownRef}>
                         <button
                             onClick={() => setDropdownOpen(!dropdownOpen)}
-                            className="nav-link relative text-sm hover:underline"
+                            className="flex nav-link relative text-sm hover:underline"
                         >
-                            🛒 Carrito ({totalItems})
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                height="24px"
+                                viewBox="0 -960 960 960"
+                                width="24px"
+                                fill="#131700"
+                            >
+                                <path d="M280-80q-33 0-56.5-23.5T200-160q0-33 23.5-56.5T280-240q33 0 56.5 23.5T360-160q0 33-23.5 56.5T280-80Zm400 0q-33 0-56.5-23.5T600-160q0-33 23.5-56.5T680-240q33 0 56.5 23.5T760-160q0 33-23.5 56.5T680-80ZM246-720l96 200h280l110-200H246Zm-38-80h590q23 0 35 20.5t1 41.5L692-482q-11 20-29.5 31T622-440H324l-44 80h480v80H280q-45 0-68-39.5t-2-78.5l54-98-144-304H40v-80h130l38 80Zm134 280h280-280Z" />
+                            </svg>
+                            ({totalItems})
                         </button>
+
 
                         {dropdownOpen && (
                             <div className="absolute right-0 mt-2 w-64 bg-white shadow-lg border rounded-lg z-50 p-4">
--- src/Pages/Layout.jsx
+++ src/Pages/Layout.jsx
@@ -6,74 +6,36 @@ import TopHeader from "../Components/TopHeader";
 
 
 export default function Layout() {
-
-  const { user, token, setUser, setToken, cart, setCart } = useContext(AppContext); //Estados globales o contextos de la aplicación
+  const { user, token, setUser, setToken } = useContext(AppContext);
   const navigate = useNavigate();
-
-  const [dropdownOpen, setDropdownOpen] = useState(false);
-  const dropdownRef = useRef();
-
-  const totalItems = cart.reduce((sum, item) => sum + item.quantity, 0);
-
-
-  // Cerrar el dropdown si clic fuera
-  useEffect(() => {
-    function handleClickOutside(e) {
-      if (dropdownRef.current && !dropdownRef.current.contains(e.target)) {
-        setDropdownOpen(false);
-      }
-    }
-    document.addEventListener("mousedown", handleClickOutside);
-    return () => document.removeEventListener("mousedown", handleClickOutside);
-  }, []);
-
 
   async function handleLogout(e) {
     e.preventDefault();
 
-    /* Petición de logout */
-    const res = await fetch('/api/logout', {
+    const res = await fetch("/api/logout", {
       method: "post",
       headers: {
         Authorization: `Bearer ${token}`,
-      }
+      },
     });
 
     const data = await res.json();
     console.log(data);
 
-    if(res.ok) {
+    if (res.ok) {
       setUser(null);
       setToken(null);
       localStorage.removeItem("token");
-      navigate('/');
+      navigate("/");
     }
   }
-
-
-  //funciones para el carrito
-  const increment = (id) => {
-    setCart(prev => prev.map(p => p.id === id ? { ...p, quantity: p.quantity + 1 } : p));
-  };
-
-  const decrement = (id) => {
-    setCart(prev =>
-      prev
-        .map(p => (p.id === id && p.quantity > 1 ? { ...p, quantity: p.quantity - 1 } : p))
-        .filter(p => p.quantity > 0)
-    );
-  };
-
-  const removeItem = (id) => {
-    setCart(prev => prev.filter(p => p.id !== id));
-  };
 
 
 
   return (
     <>
-    <TopHeader/>
-      <Header/>
+      <TopHeader />
+      <Header />
       <main>
         <Outlet /> {/** Esto renderiza las rutas hijas. */}
       </main>
